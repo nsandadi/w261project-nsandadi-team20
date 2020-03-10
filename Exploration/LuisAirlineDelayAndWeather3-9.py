28fe1154498110731661fa1f5c024428d0811e19
Diana960 <dianai@live.com>
2020-03-10 00:08:29 -0700

    Added variable summary for airline delays dataset -- need to look at weather

--- Exploration/LuisAirlineDelayAndWeather3-9.py
+++ Exploration/LuisAirlineDelayAndWeather3-9.py
@@ -98,6 +98,10 @@ mini_airlines = airlines.sample(False, 0.00001)
 
 # COMMAND ----------
 
+airlines.printSchema()
+
+# COMMAND ----------
+
 # Explore whether CRS_DEP_TIME + DEP_DELAY = DEP_TIME
 res = mini_airlines.where(airlines.DEP_TIME != (airlines.CRS_DEP_TIME + airlines.DEP_DELAY)) \
              .select('CRS_DEP_TIME', 'DEP_TIME', 'DEP_DELAY', (airlines.CRS_DEP_TIME + airlines.DEP_DELAY).alias('Sum')).collect()
@@ -163,7 +167,68 @@ airlines.select(varName).distinct().count()
 
 # COMMAND ----------
 
-airlines.printSchema()
+# MAGIC %md
+# MAGIC #### EDA Summary
+# MAGIC ##### Time-of-year data
+# MAGIC * `Year`
+# MAGIC * `Quarter` -- data well represented across quarters & years
+# MAGIC * `Month` -- data less well represented across months & years
+# MAGIC * `Day_of_Month` -- fewer records on 31st
+# MAGIC * `Day_of_week`
+# MAGIC * `FL_Date` - flight data (as a string)
+# MAGIC 
+# MAGIC ##### Airline info
+# MAGIC * `Op_Carrier_Airline_Id` - airline Id, total of 19 distinct airlines
+# MAGIC 
+# MAGIC ##### Origin Airport Info
+# MAGIC * `Origin_Airport_Id` - 370 distinct numeric values
+# MAGIC * `Origin` - 370 distinct alphanumeric values (e.g. "SEA")
+# MAGIC * `Origin_city_name` - 361 distinct values (e.g. "Seattle, WA") ----- seems odd 361 < 360...
+# MAGIC * `Origin_state_abr` - 53 distinct values (e.g. "WA")
+# MAGIC * `Origin_state_FIPS` - 53 distinct values (ranges 1-78)
+# MAGIC * `Origin_state_nm` - 53 distinct values (eg. "Washington")
+# MAGIC 
+# MAGIC ##### Destination Airport Info
+# MAGIC * `Dest_Airport_id` - 368 distinct values (numeric)
+# MAGIC * `Dest_Airort_Seq_id` - 390 distinct value (numeric) ----- seems odd don't have equivalent for origin...
+# MAGIC * `Dest` - 368 distinct values (e.g. "SEA") 
+# MAGIC * `Dest_City_Name` - 360 distinct values (e.g. "Seattle, WA") ------ seems odd 360 < 368...
+# MAGIC * `Dest_State_Abr` - 53 distinct values (e.g. "WA")
+# MAGIC * `Dest_State_FIPS` - 53 distinct values (ranges 1-78)
+# MAGIC * `Dest_State_Nm` - 53 distinct values (e.g. "Washington")
+# MAGIC 
+# MAGIC ##### Departure-Related Info
+# MAGIC * `CRS_Dep_Time` - ranges 0001 - 2359, 1421 values
+# MAGIC * `Dep_Time` - 503,662 nulls, ranges 0001 - 2400, 1441 values
+# MAGIC * `Dep_Delay` - 510,420 nulls, ranges -204 to 2672, 1614 values
+# MAGIC * `Dep_Delay_New` - 510,420 nulls, ranges 0 to 2672, 1543 values (zero-ed out all negatives)
+# MAGIC * `Dep_Del15` - 510,420 nulls, 29138841 with "0" value, 6419426 with "1" value
+# MAGIC * `Dep_Delay_Group` - 510,420 nulls, ranges -2 to 12, 16 values
+# MAGIC * `Dep_Time_Blk` - hour time blocks, no nulls, 19 values (e.g. 2300-2359); aggregated variable
+# MAGIC 
+# MAGIC ##### Arrival-Related Info
+# MAGIC * `CRS_Arr_Time` - ranges 0001 - 2359, 1440 values
+# MAGIC * `Arr_Time` - 535,504 nulls, ranges 0001-2400, 1441 values
+# MAGIC * `Arr_Delay` - 612,606 nulls, ranges -152 to 2649, 1634 values
+# MAGIC * `Arr_Delay_New` - 612,606 nulls, ranges 0 to 2649, 1529 values
+# MAGIC * `Arr_Del15` - 612,606 nulls,  28899765 with "0" value, 6556316 with "1" value
+# MAGIC * `Arr_Delay_Group` - 612,606 nulls, ranges -2 to 12, 16 values, seems to follow a normal distribution
+# MAGIC * `Arr_Time_Blk` - hour time blocks, no nulls, 19 values (e.g. 2300-2359); aggregated variable`
+# MAGIC 
+# MAGIC ##### Cancelled/Diverted Indicators
+# MAGIC * `Cancelled` - 35,545,506 not cancelled; 523,181 cancelled
+# MAGIC * `Diverted` - 35,982,737 not diverted; 85,950 diverted
+# MAGIC 
+# MAGIC ##### Elapsed Time Info
+# MAGIC * `CRS_Elapsed_Time` - 172 null values, range -60 to 948, 643 distinct values
+# MAGIC * `Acutal_Elapsed_Time` - 609,087 null values, range 14 to 1604, 730 values
+# MAGIC 
+# MAGIC ##### Flights
+# MAGIC * `Flights` - number of flights, which is always 1
+# MAGIC 
+# MAGIC ##### Distance Info
+# MAGIC * `Distance` - ranges from 21 to 5095, 1643 distinct values
+# MAGIC * `Distance_Group` - aggregated variable, ranges 1-11, 11 total values
 
 # COMMAND ----------
 
