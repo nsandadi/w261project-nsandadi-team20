44fe9ea05b99bdbfcd1cceaa9ca7eac708bc2c8c
Diana960 <dianai@live.com>
2020-03-26 17:55:26 -0700

    Update: Exploration ExplorationAirlineDelays-20GB.py ModelExploration Task1 Task2

---
+++ Exploration/EDA2-DataViz.py
@@ -0,0 +1,9 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 
+# MAGIC ### Airlines EDA using Plotly
+# MAGIC 
+# MAGIC This is a notebook for doing some basic airlines EDA using plotly (like Shaji's plots)
+
+# COMMAND ----------
+
