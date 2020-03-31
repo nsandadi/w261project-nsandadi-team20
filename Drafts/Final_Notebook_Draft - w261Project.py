cfc12b1c1543e70ba93d86308403af4d17c4a683
Diana960 <dianai@live.com>
2020-03-30 22:25:21 -0700

    Monday night done

--- Drafts/Final_Notebook_Draft - w261Project.py
+++ Drafts/Final_Notebook_Draft - w261Project.py
@@ -1,1 +1,101 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC # Final Notebook - Draft
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## I. Question Formulation
+# MAGIC 
+# MAGIC - Describe dataset high level (introduce it to give context)
+# MAGIC - Goal: Predict the event of a departure delay, given conditions of a flight (consider arrival delay as well)
+# MAGIC - Practical Departure Delay = greater than 30 minutes delay after scheduled departure time (CRS_Dep_Time)
+# MAGIC - Why perform analysis / what questions will this answer?
+# MAGIC     - Time is money
+# MAGIC     - airlines can better prepare people for delays --> make people happier by notifying them
+# MAGIC     - airlines can figure out problems and improve processes
+# MAGIC     - airports can better manage gates, air traffic control, general logistics
+# MAGIC     - can better predict arrival delay
+# MAGIC     - help people figure out what kind of flights to go on (carriers, origin, destination, dep/arrival time, distance, day of week, etc)
+# MAGIC       (what are the things people can actively change to minimize delay)
+# MAGIC - Should be better than the baseline (which is just predicting randomly with parameter p = probability of historical delay (~0.8)); should be interpretable to be practically useful
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## II. EDA & Discussion of Challenges
+# MAGIC - 2-3 EDA Tasks to help make decisions on how we implement the algorithm to be scalable
+# MAGIC     - Binning could be a way to scale decision tree algorithms (when defining splits, e.g. CRS_Dep_Time)
+# MAGIC     - More customized binning for the origin-destination to fit some custom criterion (to make new features)
+# MAGIC     - Brieman's Theorem can help with binning on categorical variables -- we could do an EDA task to rank some categorical variables (reduce number of possible splits -- bring order to unordered cateogrical variables)
+# MAGIC - Challenges anticipated based on EDA
+# MAGIC     - Cleaning/joining datasets?
+# MAGIC     - Feature Selection (or feature augmentation)
+# MAGIC     - gradient descent for logistic regression?
+# MAGIC     - sharing clusters, not being able to load entire dataset into memory (but can load fractions and/or aggregations of it!)
+# MAGIC     - store data to optimize for column/row retrieval based on algorithm (parquet for DT & avro for LR)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## III. Feature Engineering
+# MAGIC - Cleaning
+# MAGIC     - Remove cancelled/diverted flights (don't have full data)
+# MAGIC     - Remove flights with null outcomes (can't estimate)
+# MAGIC - Transformations
+# MAGIC     - Binning (elapsed time, departure/arrival time, )
+# MAGIC     - Forming Indiciator Variables (e.g. departure/arrival delay > 30 min (true/false))
+# MAGIC - Interaction Terms
+# MAGIC     - Interacting variables to make new features ("Day of Year", "Time of week")
+# MAGIC     - Binning interaction terms
+# MAGIC     - Joining interaction terms to make new features
+# MAGIC          - Day of month with month ("Day of Year") -> Holidays indiciator (or near holiday indicator)
+# MAGIC          - Departure/Arrival time day of week ("Time of week")
+# MAGIC - Treatment of Categorical Variables
+# MAGIC     - Categorical variables: Time of Year Variables, Distance Group, Carrier, origin/destination airports
+# MAGIC     - Order  Breiman's Theorem (rank by volume of flights, probability of departure delay, etc)
+# MAGIC     - Make broader categories for categorical variables (not something we've tried yet, but we can do this)
+# MAGIC     - one-hot encoding (svms)
+# MAGIC - Treatment for categorical / binned variables
+# MAGIC     - computing # of flights per category
+# MAGIC     - computing probability of delay per category
+# MAGIC     - Try a model that includes these to see if the model selects them (Decision tree)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## IV. Algorithm Exploration
+# MAGIC - Apply 2-3 Algorithms
+# MAGIC     - Logistic Regression
+# MAGIC     - SVM
+# MAGIC     - Naive Bayes
+# MAGIC     - Decision Tree
+# MAGIC - Expectations / Tradeoffs
+# MAGIC     - All able to do classificaiton (Delay/No Delay)
+# MAGIC     - LR: Interpretable (good), get estimate for effect of each variable, manual feature selection required, need to deal with multi-collinearity, among other things
+# MAGIC     - SVM: needed to transform & one-hot encode categorical variables
+# MAGIC     - DT: Not a lot of hyper parameter tuning/feature selection (most automated), 
+# MAGIC - Results
+# MAGIC 
+# MAGIC -- See Task 3 Submission :) --
+# MAGIC https://docs.google.com/document/d/1IaGOgYWSRCH-WgDzJ7N2Lw6y5h8zxpTQ8HHg9AqpML8/edit?usp=sharing
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## V. Algorithm Implementation
+# MAGIC - Toy example likely with a decision tree on a mini_mini_train dataset (like 10 rows)
+# MAGIC - Walk through training the model and doing inference
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## VI. Conclusions
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## V. Applications of Course Concepts
+
+# COMMAND ----------
+
